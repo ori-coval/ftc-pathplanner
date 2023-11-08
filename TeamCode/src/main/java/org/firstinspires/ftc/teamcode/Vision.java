5871bc1616b0f89f038ea4c30bc5dc4c90fc71f2
itay12016 <128303898+itay12016@users.noreply.github.com>
2023-11-08 19:23:49 +0200

    itay- Started working on image recognition (teamProp)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
@@ -1,6 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+
 public class Vision {
+    private VisionPortal visionPortal;
+    private TfodProcessor tfod;
+    private WebcamName webcamName;
+    public Vision(WebcamName webcamName){
+        this.webcamName = webcamName;
+
+        tfod = new TfodProcessor.Builder().build();
+
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        builder.setCamera(webcamName);
+        builder.addProcessor(tfod);
+        visionPortal = builder.build();
+    }
+
+    public int getAmountOfDetections(){
+        return tfod.getRecognitions().size();
+    }
+
+
 
 
 
