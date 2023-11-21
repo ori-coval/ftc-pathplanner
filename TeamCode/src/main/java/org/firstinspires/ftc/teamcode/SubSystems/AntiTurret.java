cddd5ccee54a96506b9fc1c0eb243dc3d98c1b00
YitzhakK <128069616+YitzhakK@users.noreply.github.com>
2023-11-21 19:13:54 +0200

    Yitzhak-creating the class anti turret

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/AntiTurret.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.SubSystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
+
+import org.checkerframework.checker.units.qual.A;
+
+public class AntiTurret extends SubsystemBase {
+    private Servo servo;
+
+    public AntiTurret(Servo servo) {
+        this.servo = servo;
+    }
+
+    public void setPosition(double angle) {
+        servo.setPosition(angle / 360);
+    }
+
+    public double getPosition() {
+        return servo.getPosition();
+    }
+}
