372326174351da5c7e3380fc07b10f833b0c9192
tal frisch <yknthv00@gmail.com>
2023-11-27 17:17:03 +0200

    tal - fixed merged conflicts

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Turret.java
@@ -8,32 +8,18 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 public class Turret extends SubsystemBase {
     CRServo turretServoA;
     CRServo turretServoB;
-<<<<<<< HEAD
-    DcMotorEx fL;
-    
-    final double offset = 0;
-    public Turret(CRServo turretMotorA, CRServo turretMotorB, AnalogInput encoder){
-        this.turretServoA = turretMotorA;
-        this.turretServoB = turretMotorB;
-=======
     DcMotor encoder;
-    final double offset = 0;
     public Turret(CRServo turretMotorA, CRServo turretMotorB, DcMotor encoder){
         this.turretServoA = turretMotorA;
         this.turretServoB = turretMotorB;
         this.encoder = encoder;
->>>>>>> master
     }
     public void setPower (double power) {
         turretServoA.setPower(power);
         turretServoB.setPower(power);
     }
     public double getEncoderValue(){
-<<<<<<< HEAD
-        return fL.getCurrentPosition();
-=======
         return encoder.getCurrentPosition();
->>>>>>> master
     }
 
     public void stop(){
