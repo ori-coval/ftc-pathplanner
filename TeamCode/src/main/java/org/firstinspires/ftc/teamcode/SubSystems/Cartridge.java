e6a87d129221909ffeb3967ac9f39602c68fe80d
Tamirperezt <141968090+Tamirperezt@users.noreply.github.com>
2023-11-27 20:19:03 +0200

    tamir- added cartridge

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Cartridge.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.SubSystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Cartridge extends SubsystemBase {
+    public enum State{
+        CLOSED(0), SEMI_OPEN(0.125), OPEN(0.25);
+        final double position;
+        State(double position){
+            this.position = position;
+        }
+    }
+
+    private Servo latch;
+    private State lastState = State.CLOSED;
+
+    public Cartridge(Servo latch){
+        this.latch = latch;
+    }
+
+
+    public void setState(State state){
+        latch.setPosition(state.position);
+        lastState = state;
+    }
+
+    public State getState(){
+        return lastState;
+    }
+
+
+
+
+
+
+
+}
