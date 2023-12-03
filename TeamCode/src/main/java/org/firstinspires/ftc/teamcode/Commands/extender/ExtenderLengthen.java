41ea2897014637bb7823af3f3004047b51d99c46
unknown <mishmashbgu@gmail.com>
2023-12-03 17:28:27 +0200

    tair - finish extender lengthen

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/extender/ExtenderLengthen.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/extender/ExtenderLengthen.java
@@ -2,5 +2,19 @@ package org.firstinspires.ftc.teamcode.Commands.extender;
 
 import com.arcrobotics.ftclib.command.CommandBase;
 
+import org.firstinspires.ftc.teamcode.SubSystems.Extender;
+
 public class ExtenderLengthen extends CommandBase {
+    private Extender extender;
+    private final double position0 = 0;
+    private final double position1 = 0;
+    private final double position2 =0;
+    public ExtenderLengthen(Extender extender){
+        this.extender = extender;
+        addRequirements(extender);
+    }
+
+    public void setPosition(double position){
+        extender.setPosition(position);
+    }
 }
