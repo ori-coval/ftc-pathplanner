492ea41c204c2c287943c2c6d4a593762a2e7be8
tal frisch <yknthv00@gmail.com>
2023-12-18 17:41:25 +0200

    tair - wrote arm position

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmPosition.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+public enum ArmPosition {
+
+    SCORE_BOTTOM_CLOSE(0, 0, 0, true),
+    SCORE_BOTTOM_MID(0, 0, 0, true),
+    SCORE_BOTTOM_FAR(0, 0, 0, true),
+    SCORE_MID_CLOSE(0, 0, 0, true),
+    SCORE_MID_MID(0, 0, 0, true),
+    SCORE_MID_FAR(0, 0, 0, true),
+    SCORE_TOP_CLOSE(0, 0, 0, true),
+    SCORE_TOP_MID(0, 0, 0, true),
+    SCORE_TOP_FAR(0, 0, 0, true),
+    INTAKE(0, 0, 0, false);
+
+    private final double elevatorHeight;
+    private final double extenderLength;
+    private final double turretAngle;
+    private final boolean elbowIsScoring;
+
+    ArmPosition(double elevatorHeight, double extenderLength, double turretAngle, boolean elbowIsScoring) {
+        this.elevatorHeight = elevatorHeight;
+        this.elbowIsScoring = elbowIsScoring;
+        this.turretAngle = turretAngle;
+        this.extenderLength = extenderLength;
+
+    }
+
+    public double getElevatorHeight() {
+        return elevatorHeight;
+    }
+
+    public double getExtenderLength() {
+        return extenderLength;
+    }
+
+    public double getTurretAngle(boolean isRightOfBoard) {
+        return turretAngle * (isRightOfBoard ? 1 : -1);
+    }
+}
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Extender.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Extender.java
@@ -4,25 +4,14 @@ import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Extender extends SubsystemBase {
-
-    public enum Length {
-        CLOSED(0), MID_WAY(0.25), OPEN(0.5);
-        private final double servoPosition;
-        Length(double length){
-            this.servoPosition = length;
-        }
-    }
     private Servo linearServo;
-    private Length currentLength;
-
     public Extender (Servo linearServo){
         this.linearServo = linearServo;
     }
-    public void setPosition(Length length){
-        linearServo.setPosition(length.servoPosition);
-        currentLength = length;
+    public void setLength(double length){
+        linearServo.setPosition(length);
     }
-    public Length getPosition(){
-        return currentLength;
+    public double getLength(){
+        return linearServo.getPosition();
     }
 }
