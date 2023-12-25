6ffe01b44478108c7a829b29f3a38a5cfba72798
unknown <mishmashbgu@gmail.com>
2023-12-25 19:55:20 +0200

    tair- change to cluster way of ArmGetToPos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmPosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmPosition.java
@@ -2,28 +2,35 @@ package org.firstinspires.ftc.teamcode;
 
 public enum ArmPosition {
 
-    SCORE_BOTTOM_CLOSE(0, 0, 0, 0),
-    SCORE_BOTTOM_FAR(0, 0, 0, 0),
-    SCORE_MID_CLOSE(0, 0, 0, 0),
-    SCORE_MID_FAR(0, 0, 0, 0),
-    SCORE_TOP_CLOSE(0, 0, 0, 0),
-    SCORE_TOP_FAR(0, 0, 0, 0),
-    SCORE_FRONT_TOP(0,0,0,0),
-    SCORE_FRONT_MID(0,0,0,0),
-    SCORE_FRONT_BOTTOM(0,0,0,0),
-    INTAKE(0, 0, 0,0);
+    SCORE_BOTTOM_CLOSE(0, 0, 0, 0, Cluster.cluster0),
+    SCORE_BOTTOM_FAR(0, 0, 0, 0, Cluster.cluster0),
+    SCORE_MID_CLOSE(0, 0, 0, 0, Cluster.cluster0),
+    SCORE_MID_FAR(0, 0, 0, 0, Cluster.cluster0),
+    SCORE_TOP_CLOSE(0, 0, 0, 0, Cluster.cluster0),
+    SCORE_TOP_FAR(0, 0, 0, 0, Cluster.cluster0),
+    SCORE_FRONT_TOP(0,0,0,0, Cluster.cluster0),
+    SCORE_FRONT_MID(0,0,0,0, Cluster.cluster0),
+    SCORE_FRONT_BOTTOM(0,0,0,0, Cluster.cluster0),
+    INTAKE(0, 0, 0,0, Cluster.cluster0),
+    SAFE_PLACE(0,0,0,0,Cluster.cluster0);
 
     private final double elevatorHeight;
     private final double extenderLength;
     private final double turretAngle;
     private final double elbowAngle;
+    private final Cluster cluster;
 
-    ArmPosition(double elevatorHeight, double extenderLength, double turretAngle, double elbowAngle) {
+
+    private enum Cluster{
+        cluster0,
+        cluster1
+    }
+    ArmPosition(double elevatorHeight, double extenderLength, double turretAngle, double elbowAngle, Cluster cluster) {
         this.elevatorHeight = elevatorHeight;
         this.elbowAngle = elbowAngle;
         this.turretAngle = turretAngle;
         this.extenderLength = extenderLength;
-
+        this.cluster = cluster;
     }
 
     public double getElevatorHeight() {
@@ -38,5 +45,8 @@ public enum ArmPosition {
     }
 
     public double getElbowAngle() {return elbowAngle;}
+    public Cluster getCluster(){
+        return cluster;
+    }
 }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/multisystem/UnsafeMoveArmParallel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/multisystem/UnsafeMoveArmParallel.java
@@ -1,8 +1,6 @@
 package org.firstinspires.ftc.teamcode.Commands.multisystem;
 
-import com.arcrobotics.ftclib.command.ConditionalCommand;
 import com.arcrobotics.ftclib.command.ParallelCommandGroup;
-import com.arcrobotics.ftclib.command.WaitUntilCommand;
 
 import org.firstinspires.ftc.teamcode.ArmPosition;
 import org.firstinspires.ftc.teamcode.Commands.elbow.ElbowGetToAnglePID;
@@ -14,28 +12,22 @@ import org.firstinspires.ftc.teamcode.SubSystems.Elevator;
 import org.firstinspires.ftc.teamcode.SubSystems.Extender;
 import org.firstinspires.ftc.teamcode.SubSystems.Turret;
 
-public class ArmGetToPosition extends ParallelCommandGroup {
+public class UnsafeMoveArmParallel extends ParallelCommandGroup {
     private Elevator elevator;
     private Extender extender;
     private Elbow elbow;
     private Turret turret;
-    private final double ANGLE_THRESHOLD = 0;
-    private static ArmPosition lastPosition = ArmPosition.INTAKE;
 
-    public ArmGetToPosition(Elevator elevator, Elbow elbow, Extender extender, Turret turret, ArmPosition position, boolean isLeftOfBoard) {
-        lastPosition = position;
+    public UnsafeMoveArmParallel (Elevator elevator, Elbow elbow, Extender extender, Turret turret, ArmPosition position, boolean isLeftOfBoard) {
         this.elevator = elevator;
         this.extender = extender;
         this.elbow = elbow;
         this.turret = turret;
         addCommands(
-                new ConditionalCommand(
-                        new UnsafeMoveArmParallel(elevator, elbow, extender, turret, position, isLeftOfBoard),
-                        new UnsafeMoveArmParallel(elevator, elbow, extender, turret, ArmPosition.SAFE_PLACE, isLeftOfBoard).andThen(
-                                new UnsafeMoveArmParallel(elevator, elbow, extender, turret, position, isLeftOfBoard)
-                        ),
-                        ()-> lastPosition.getCluster()==position.getCluster()
-                )
+                new ElevatorGetToHeightPID(elevator, position.getElevatorHeight()),
+                new RotateTurretByPID(turret, position.getTurretAngle(isLeftOfBoard)),
+                new ElbowGetToAnglePID(elbow, position.getElbowAngle()),
+                new ExtenderSetLength(extender, position.getExtenderLength())
         );
     }
 }
