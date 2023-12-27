5ac522fe3861662f560711801bea142e0ed6f6bf
itay12016 <128303898+itay12016@users.noreply.github.com>
2023-12-27 21:11:09 +0200

    itay- find the last value

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/intake/OnlyForTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/intake/OnlyForTesting.java
@@ -6,23 +6,26 @@ import org.firstinspires.ftc.teamcode.SubSystems.InTake;
 
 public class OnlyForTesting extends CommandBase {
     private InTake inTake;
-    public double position;
-
-    public OnlyForTesting(InTake inTake, double position) {
-        this.inTake = inTake;
-        this.position = position;
-        this.addRequirements(inTake);
-    }
+    private double target = inTake.STACK_POSITION[5];
 
     // the first value to 5 piksel is 0.62
     // the second value to 4 piksel is 0.58
     // the third value to 3 piksel is 0.55
     // the fourth value to 2 piksel is 0.52
+    // the fifth value to 1 piksel is 0.49
     // כשהאיסוף למעלה 0.67
+
+    public OnlyForTesting(InTake inTake) {
+        this.inTake = inTake;
+        this.addRequirements(inTake);
+    }
+
     @Override
     public void initialize() {
-        inTake.setPosition(position);
+
     }
+
+
 
     @Override
     public void end(boolean interrupted) {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/InTake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/InTake.java
@@ -9,14 +9,21 @@ public class InTake extends SubsystemBase {
     private Servo inTakeAngle;
     public final double COLLECT_POWER = 0.8;
     public final double EJECT_POWER = -0.9;
+    public final double STACK_POSITION[] = {0.49, 0.52, 0.55, 0.58, 0.62};
+    private double target = STACK_POSITION[5];
 
 
+    // the first value to 5 piksel is 0.62
+    // the second value to 4 piksel is 0.58
+    // the third value to 3 piksel is 0.55
+    // the fourth value to 2 piksel is 0.52
+    // the fifth value to 1 piksel is 0.49
+    // כשהאיסוף למעלה 0.67
 
     public InTake(DcMotor inTakeMotor, Servo inTakeAngle){
         this.inTakeMotor = inTakeMotor;
         this.inTakeAngle = inTakeAngle;
     }
-
     public void setPower(double power){
         inTakeMotor.setPower(power);
     }
@@ -30,4 +37,8 @@ public class InTake extends SubsystemBase {
     }
     public double getPosition(){return inTakeAngle.getPosition();}
 
+    @Override
+    public void periodic() {
+
+    }
 }
