c8b52735d3668c8d0e8a6ae0e3d1050429c8c687
YitzhakK <128069616+YitzhakK@users.noreply.github.com>
2024-01-24 18:22:51 +0200

    yitzhak- position command, not tested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/intake/IntakePosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/intake/IntakePosition.java
@@ -1,4 +1,29 @@
 package org.firstinspires.ftc.teamcode.Commands.intake;
 
-public class IntakePosition {
+import com.arcrobotics.ftclib.command.ConditionalCommand;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+
+import org.firstinspires.ftc.teamcode.ArmPosition;
+import org.firstinspires.ftc.teamcode.SubSystems.InTake;
+
+public class IntakePosition extends ConditionalCommand {
+
+    public IntakePosition(InTake inTake){
+        super(
+              new SequentialCommandGroup(
+                      new WaitCommand(10),
+                      new InstantCommand(()->inTake.setStackPosition(2)),
+                      new WaitCommand(10),
+                      new InstantCommand(()->inTake.setStackPosition(1)),
+                      new WaitCommand(10),
+                      new InstantCommand(()->inTake.setStackPosition(0))
+
+              ),
+              new InstantCommand(()->inTake.setStackPosition(3)),
+                ()-> inTake.getStackPosition() != 3
+        );
+    }
+
 }
