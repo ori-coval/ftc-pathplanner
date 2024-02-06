e7769a5b2137e7d1e4d4ccac54c5479ea7f9ffbf
unknown <mishmashbgu@gmail.com>
2024-02-06 15:45:13 +0200

    tair- fix problems with intake commands

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/auto/GoCollectPixelFromSpikeMark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/auto/GoCollectPixelFromSpikeMark.java
@@ -3,24 +3,24 @@ package org.firstinspires.ftc.teamcode.Commands.auto;
 
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 
+import org.firstinspires.ftc.teamcode.Commands.intakeLifter.IntakeSetStackPosition;
+import org.firstinspires.ftc.teamcode.Commands.intakeRoller.IntakeUntilFull;
 import org.firstinspires.ftc.teamcode.Commands.utils.SideCommandSwitch;
-import org.firstinspires.ftc.teamcode.Commands.intake.IntakeSetPosition;
-import org.firstinspires.ftc.teamcode.Commands.multiSystem.IntakeUntilFull;
 import org.firstinspires.ftc.teamcode.RoadRunner.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.Side;
 import org.firstinspires.ftc.teamcode.SubSystems.Intake;
 
-public class CollectPixelFromSpikeMark extends SequentialCommandGroup {
-    public CollectPixelFromSpikeMark(SampleMecanumDrive driveTrain, Intake intake, Side side) {
+public class GoCollectPixelFromSpikeMark extends SequentialCommandGroup {
+    public GoCollectPixelFromSpikeMark(SampleMecanumDrive driveTrain, Intake intake, Side side) {
         super(
-                new IntakeSetPosition(intake,3),
+                new IntakeSetStackPosition(intake.lifter,3),
                 new SideCommandSwitch(
                         new TrajectoryFollowerCommand(Trajectories.get("Driving to stack while avoiding pixel on Left"), driveTrain),
                         new TrajectoryFollowerCommand(Trajectories.get("Driving to stack while avoiding pixel on Center"), driveTrain),
                         new TrajectoryFollowerCommand(Trajectories.get("Driving to stack while avoiding pixel on Right"), driveTrain),
                         () -> side) ,
-                new IntakeSetPosition(intake, 2),
-                new IntakeUntilFull(intake).withTimeout(10000)
+                new IntakeSetStackPosition(intake.lifter, 2),
+                new IntakeUntilFull(intake.roller).withTimeout(10000)
         );
     }
 }
