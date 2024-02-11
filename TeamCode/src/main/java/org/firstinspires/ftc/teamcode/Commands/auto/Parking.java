73a43ac7d2f04e578da26f320cbdeda073dd227e
itay12016 <128303898+itay12016@users.noreply.github.com>
2024-02-11 20:55:47 +0200

    tair&itay - parking

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/auto/Parking.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.Commands.auto;
+
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+
+import org.firstinspires.ftc.teamcode.ArmPosition;
+import org.firstinspires.ftc.teamcode.Commands.multiSystem.ArmGetToPosition;
+import org.firstinspires.ftc.teamcode.RoadRunner.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.SubSystems.AntiTurret;
+import org.firstinspires.ftc.teamcode.SubSystems.Cartridge;
+import org.firstinspires.ftc.teamcode.SubSystems.Elbow;
+import org.firstinspires.ftc.teamcode.SubSystems.Elevator;
+import org.firstinspires.ftc.teamcode.SubSystems.Extender;
+import org.firstinspires.ftc.teamcode.SubSystems.Turret;
+
+public class Parking extends SequentialCommandGroup {
+    public Parking(SampleMecanumDrive driveTrain, Elevator elevator, Extender extender, Elbow elbow, Turret turret, AntiTurret antiTurret, Cartridge cartridge){
+        super(
+            new ParallelCommandGroup(new TrajectoryFollowerCommand(Trajectories.get("1/2 meter backwards from backstage"), driveTrain)),
+            new ArmGetToPosition(elevator, elbow, extender, turret, antiTurret, ArmPosition.INTAKE, true),
+            new ParallelCommandGroup(new TrajectoryFollowerCommand(Trajectories.get("1/2 meter forwards to backstage"), driveTrain))
+
+        );
+    }
+}
