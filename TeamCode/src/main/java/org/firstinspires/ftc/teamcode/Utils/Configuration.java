d6386bf76214de69a8f396deae56a4b4698e01cc
NevoQ <nevoday2006@gmail.com>
2024-02-28 20:34:34 +0200

    Nevo - configuration fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Configuration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Configuration.java
@@ -10,10 +10,9 @@ public class Configuration {
     public static final String TURRET_ENCODER = DRIVE_TRAIN_FRONT_RIGHT;
     public static final String INTAKE_SERVO = "intakeServo";
     public static final String INTAKE_MOTOR = "intake";
-    public static final String INTAKE_SWITCH = "switch";
+    public static final String INTAKE_SWITCH = "conveyorSwitch";
     public static final String ELEVATOR_RIGHT = "elevatorRight";
     public static final String ELEVATOR_LEFT = "elevatorLeft";
-    public static final String ELEVATOR_CLIMBER = "elevatorClimber";
     public static final String ELBOW_RIGHT = "elbowRight";
     public static final String ELBOW_LEFT = "elbowLeft";
     public static final String CARTRIDGE = "cartridge";
@@ -57,8 +56,8 @@ public class Configuration {
             MOTORS:
                     0: elevatorLeft            (GoBilda 5202/3/4 series)            (dcMotor)
                     1: elevatorRight           (GoBilda 5202/3/4 series)            (dcMotor)
-                    2: turret                  (GoBilda 5202/3/4 series)            (dcMotor)
-                    3: intake                  (GoBilda 5202/3/4 series)            (dcMotor)
+                    2: intake                  (GoBilda 5202/3/4 series)            (dcMotor)
+                    3: turret                  (GoBilda 5202/3/4 series)            (dcMotor)
 
             SERVOS:
                     0: elbowRight            (servo)                              (servo)
