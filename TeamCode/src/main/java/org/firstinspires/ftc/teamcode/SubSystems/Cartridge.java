41f83b9e5610dab17ee540d5c0771631d001eead
NevoQ <nevoday2006@gmail.com>
2024-03-03 17:00:40 +0200

    Nevo - new cartridge positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Cartridge.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Cartridge.java
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.teamcode.Utils.Configuration;
 
 public class Cartridge extends SubsystemBase {
     public enum State {
-        CLOSED(0.68), SEMI_OPEN(0.45), OPEN(0.3); //TODO: NEW CARTRIDGE POSITIONS
+        CLOSED(0.93), SEMI_OPEN(0.75), OPEN(0.3);
         final double position;
         State(double position){
             this.position = position;
