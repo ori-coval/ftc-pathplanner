f4a32d24fd87579ff23c88bcf61ee8a164db2d71
NevoQ <nevoday2006@gmail.com>
2024-03-07 20:55:49 +0200

    Nevo - new cartridge pos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Cartridge.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Cartridge.java
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.teamcode.Utils.Configuration;
 
 public class Cartridge extends SubsystemBase {
     public enum State {
-        CLOSED_ONE_PIXEL(1), CLOSED_TWO_PIXELS(0.9), SEMI_OPEN(0.81), OPEN(0.72), INTAKE_OPEN(0.5); //TODO: NEW CARTRIDGE POS
+        CLOSED_ONE_PIXEL(0.9), CLOSED_TWO_PIXELS(0.82), SEMI_OPEN(0.81), OPEN(0.585), INTAKE_OPEN(0.5); //TODO: NEW CARTRIDGE POS
         final double position;
         State(double position){
             this.position = position;
