c441df7cd12b44e7f61f3c542f66cf44f35b5da4
Nevo <nevoday2006@gmail.com>
2024-06-16 06:41:07 +0300

    files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CuttlefishFTCBridge/src/devices/CuttleEncoder.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.CuttlefishFTCBridge.src.devices;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.roboctopi.cuttlefish.components.RotaryEncoder;
+import com.roboctopi.cuttlefish.utils.Direction;
+
+/**
+ * Rotary encoder connected through a motor encoder port
+ * */
+public class CuttleEncoder implements RotaryEncoder
+{
+    public CuttleRevHub hub;
+    private final double encTicks;
+    private int direction = 1;
+    public int mPort;
+
+    double home = 0.0;
+
+    /**
+     * @param revHub
+     * @param port Motor port of the encoder
+     * @param countsPerRevolution Number of counts per revolution of the encoder
+     * */
+    public CuttleEncoder(CuttleRevHub revHub, int port, double countsPerRevolution) {
+        hub = revHub;
+        encTicks = countsPerRevolution;
+        mPort = port;
+        home += getRotation()*direction;
+    }
+
+    /**
+     * Get the rotation of the encoder in radians
+     * */
+    public double getRotation()
+    {
+        return (2*Math.PI*getCounts()/encTicks - home)*direction;
+    }
+    public double getVelocity()
+    {
+        return 2*Math.PI*hub.bulkData.getEncoderVelocity(mPort)/encTicks*direction;
+    }
+
+    /**
+     * Get the number of counts that the encoder has turned
+     * */
+    public int getCounts()
+    {
+        return hub.bulkData.getEncoderPosition(mPort);
+    }
+
+
+    /**
+     * Set the direction of the encoder.
+     * @param direction
+     * */
+    public void setDirection(Direction direction)
+    {
+        if(direction == Direction.REVERSE)
+        {
+            this.direction = -1;
+        }
+        else
+        {
+            this.direction = 1;
+        }
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CuttlefishFTCBridge/src/utils/CurrentAlliance.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.CuttlefishFTCBridge.src.utils;
+
+public class CurrentAlliance {
+    public enum AllianceTypes {
+        RED,
+        BLUE
+    }
+
+    public static AllianceTypes alliance = AllianceTypes.RED;
+}
