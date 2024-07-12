b8a6a29c63056cb05700c3deb75cbfb1e3b23959
Nevo <nevoday2006@gmail.com>
2024-07-12 22:17:14 +0300

    DeferredCommand on kt for practice

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MMLib/Utils/MMDeferredCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MMLib/Utils/MMDeferredCommand.java
@@ -5,24 +5,26 @@ import com.arcrobotics.ftclib.command.CommandBase;
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.Subsystem;
 
+import org.firstinspires.ftc.teamcode.MMRobot;
+
 import java.util.function.Supplier;
 
 public class MMDeferredCommand extends CommandBase {
 
     private final Command nullCommand = new InstantCommand();
 
-    private final Supplier<Command> supplier;
+    private final Supplier<Command> commandSupplier;
     private Command command = nullCommand;
 
 
-    public MMDeferredCommand(Supplier<Command> command, Subsystem... requireSubsystems) {
-        this.supplier = MMUtils.requireNonNullParam(command, "Supplier<Command>", "MMDeferredCommand");
+    public MMDeferredCommand(Supplier<Command> commandSupplier, Subsystem... requireSubsystems) {
+        this.commandSupplier = MMUtils.requireNonNullParam(commandSupplier, "Supplier<Command>", "MMDeferredCommand");
         addRequirements(requireSubsystems);
     }
 
     @Override
     public void initialize() {
-        Command cmd = supplier.get();
+        Command cmd = commandSupplier.get();
         if(cmd != null) command = cmd;
         command.initialize();
     }
