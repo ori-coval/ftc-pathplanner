99093c6c018a94ef14a5553f9d46fa6a0108e402
mishmash <lahavnave8@gmail.com>
2024-10-11 05:03:13 +0300

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/LinearIntakeCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/LinearIntakeCommand.java
@@ -4,24 +4,31 @@ import com.arcrobotics.ftclib.command.CommandBase;
 import com.arcrobotics.ftclib.command.button.Trigger;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 
+import org.firstinspires.ftc.teamcode.Libraries.MMLib.Utils.MMRange;
+import org.firstinspires.ftc.teamcode.Libraries.MMLib.Utils.MMUtils;
 import org.firstinspires.ftc.teamcode.MMRobot;
-
-import java.util.function.DoubleSupplier;
 
 public class LinearIntakeCommand extends CommandBase{
     Trigger trigger;
-    DoubleSupplier position;
+    private final double TRIG_INPUT_START = 0;
+    private final double TRIG_INPUT_END = 0;
+    private final double TRIG_OUTPUT_START = 0;
+    private final double TRIG_OUTPUT_END = 0.8;
 
-    public LinearIntakeCommand(Trigger trigger, DoubleSupplier position){
-        this.position = position;
+    public LinearIntakeCommand(Trigger trigger){
         this.trigger = trigger;
         addRequirements(MMRobot.getInstance().mmSystems.linearIntake);
     }
 
-
     @Override
     public void execute() {
-        MMRobot.getInstance().mmSystems.linearIntake.setPosition(position.getAsDouble());
+        MMRobot.getInstance().mmSystems.linearIntake.setPosition(
+                MMUtils.mapValuesLinearByRange(
+                        MMRobot.getInstance().mmSystems.gamepadEx1.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER),
+                        new MMRange(TRIG_INPUT_START, TRIG_INPUT_END),
+                        new MMRange(TRIG_OUTPUT_START, TRIG_OUTPUT_START)
+                )
+        );
     }
 
     @Override
