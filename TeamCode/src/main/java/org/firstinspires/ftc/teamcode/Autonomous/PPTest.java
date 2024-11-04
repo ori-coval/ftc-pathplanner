2d3e13bd03e11a5529aa583683c0feebdcd9808f
ori <oricoval@gmail.com>
2024-11-04 22:12:51 +0200

    changed some shit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PPTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PPTest.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.missingWpilibClasses.math.kinematics.ChassisSpeeds;
 import com.pathplanner.lib.path.PathPlannerPath;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -11,8 +12,6 @@ import org.firstinspires.ftc.teamcode.Utils.OpModeType;
 
 @TeleOp(name = "PPtest")
 public class PPTest extends MMTeleOp {
-
-
     AutoDrivetrain autoDrivetrain;
     public PPTest() {
         super(OpModeType.NonCompetition.EXPERIMENTING_NO_EXPANSION);
@@ -25,16 +24,20 @@ public class PPTest extends MMTeleOp {
 
         waitForStart();
 
-        AutoBuilder.followPath(PathPlannerPath.fromPathFile("test")).schedule();
-
+//        AutoBuilder.followPath(PathPlannerPath.fromPathFile("test")).schedule();
+    autoDrivetrain.drive(0.5);
 
     }
 
     @Override
     public void run() {
         super.run();
+        autoDrivetrain.odometry.update();
+        FtcDashboard.getInstance().getTelemetry().addData("x",autoDrivetrain.odometry.getPosX());
+        FtcDashboard.getInstance().getTelemetry().addData("y",autoDrivetrain.odometry.getPosY());
+        FtcDashboard.getInstance().getTelemetry().addData("heading",autoDrivetrain.odometry.getHeading());
 
-        FtcDashboard.getInstance().getTelemetry().addData("x", autoDrivetrain.odometry.getPosX());
+        FtcDashboard.getInstance().getTelemetry().addData("vel", autoDrivetrain.odometry.getVelX());
         FtcDashboard.getInstance().getTelemetry().update();
     }
 }
